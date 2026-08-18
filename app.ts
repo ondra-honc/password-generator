cf13d3ee43a3936777d5e7a06d6cf91393aa34ad
Ondřej Honc <ondra.honc1@gmail.com>
2026-08-18 18:43:32 +0200

    feat: generate password button

--- app.ts
+++ app.ts
@@ -4,17 +4,45 @@ function generateSecureBytes(size: number): Uint8Array {
     return array;
 }
 
+function generatePassword(length: number, uppercaseLetter?: boolean, numbers?: boolean, symbols?: boolean): string {
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+    let allowedChars = lowercaseChars;
+    if (uppercaseLetter) allowedChars += uppercaseChars;
+    if (numbers) allowedChars += numberChars;
+    if (symbols) allowedChars += symbolChars;
+
+    const array = generateSecureBytes(length);
+    let password = '';
+    
+    for (let i: number = 0; i < array.length; i++) {
+        const randomIndex = array[i]! % allowedChars.length;
+        password += allowedChars[randomIndex];
+    }
+
+    return password
+}
+
 const copyButton = document.getElementById("copy-button") as HTMLButtonElement;
-const passwordLength = document.getElementById("password-length") as HTMLInputElement;
-const uppercase = document.getElementById("uppercase") as HTMLInputElement;
-const numbers = document.getElementById("numbers") as HTMLInputElement;
-const symbols = document.getElementById("symbols") as HTMLInputElement;
+const generateButton = document.getElementById("generate") as HTMLButtonElement;
+const outputPassword = document.getElementById("outputarea") as HTMLOutputElement;
 
 copyButton.addEventListener('click', () => {
-    const outputPassword = document.getElementById("outputarea") as HTMLOutputElement;
     const text = outputPassword.textContent?.trim() || "";
 
     if (text === "Here will be your password" || text === '') return;
 
     navigator.clipboard.writeText(text);
+})
+
+generateButton.addEventListener('click', () => {
+    const passwordLength = document.getElementById("password-length") as HTMLInputElement;
+    const uppercase = document.getElementById("uppercase") as HTMLInputElement;
+    const numbers = document.getElementById("numbers") as HTMLInputElement;
+    const symbols = document.getElementById("symbols") as HTMLInputElement;
+
+    outputPassword.textContent = generatePassword(Number(passwordLength.value), uppercase.checked, numbers.checked, symbols.checked);
 })
--- out/app.js
+++ out/app.js
@@ -3,17 +3,41 @@ function generateSecureBytes(size) {
     globalThis.crypto.getRandomValues(array);
     return array;
 }
+function generatePassword(length, uppercaseLetter, numbers, symbols) {
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    let allowedChars = lowercaseChars;
+    if (uppercaseLetter)
+        allowedChars += uppercaseChars;
+    if (numbers)
+        allowedChars += numberChars;
+    if (symbols)
+        allowedChars += symbolChars;
+    const array = generateSecureBytes(length);
+    let password = '';
+    for (let i = 0; i < array.length; i++) {
+        const randomIndex = array[i] % allowedChars.length;
+        password += allowedChars[randomIndex];
+    }
+    return password;
+}
 const copyButton = document.getElementById("copy-button");
-const passwordLength = document.getElementById("password-length");
-const uppercase = document.getElementById("uppercase");
-const numbers = document.getElementById("numbers");
-const symbols = document.getElementById("symbols");
+const generateButton = document.getElementById("generate");
+const outputPassword = document.getElementById("outputarea");
 copyButton.addEventListener('click', () => {
-    const outputPassword = document.getElementById("outputarea");
     const text = outputPassword.textContent?.trim() || "";
     if (text === "Here will be your password" || text === '')
         return;
     navigator.clipboard.writeText(text);
+});
+generateButton.addEventListener('click', () => {
+    const passwordLength = document.getElementById("password-length");
+    const uppercase = document.getElementById("uppercase");
+    const numbers = document.getElementById("numbers");
+    const symbols = document.getElementById("symbols");
+    outputPassword.textContent = generatePassword(Number(passwordLength.value), uppercase.checked, numbers.checked, symbols.checked);
 });
 export {};
 //# sourceMappingURL=app.js.map
